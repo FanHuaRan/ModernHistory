6732c76cf81c168ca94dd92729e98c56adf9966a
FanHuaRan <834171100@qq.com>
2017-07-01 00:00:01 +0800

    2017/06/30 fhr initial

--- ModernHistory/Fhr.ModernHistory.Models/MhUser.cs
+++ ModernHistory/Fhr.ModernHistory.Models/MhUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,7 @@ namespace Fhr.ModernHistory.Models
         /// <summary>
         /// 邮箱
         /// </summary>
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",ErrorMessage = "Email is is not valid.")]
         public string Email { get; set; }
     }
 }
